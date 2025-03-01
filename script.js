b8a1e5e6823d8d10b50dc7d009739b3eecc4cefc
Andre Maier <maierandre43@gmail.com>
2025-03-01 03:30:25 +0100

    adding fixes to contacts and adding simple logo

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const slider = document.querySelector(".soft-skills-slider");
+    const softSlider = document.querySelector(".soft-skills-slider");
+    const hardSlider = document.querySelector(".hard-skills-slider");
 
     // Clone the icons to create a seamless infinite loop
-    const items = [...slider.children];
-    items.forEach((item) => {
-        const clone = item.cloneNode(true);
-        slider.appendChild(clone);
-    });
+    function duplicateItems(slider) {
+        const items = [...slider.children];
+        items.forEach((item) => {
+            const clone = item.cloneNode(true);
+            slider.appendChild(clone);
+        });
+    }
+
+    duplicateItems(softSlider);
+    duplicateItems(hardSlider);
 });
